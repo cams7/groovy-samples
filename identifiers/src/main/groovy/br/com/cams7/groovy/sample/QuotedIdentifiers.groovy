0edfa5143aa07874085fbc84b57a777743726a8e
César Magalhães <ceanma@gmail.com>
2015-08-05 12:33:50 -0300

    Foi executado o exemplo '1.1.4. Strings'

--- identifiers/src/main/groovy/br/com/cams7/groovy/sample/QuotedIdentifiers.groovy
+++ identifiers/src/main/groovy/br/com/cams7/groovy/sample/QuotedIdentifiers.groovy
@@ -2,7 +2,8 @@ package br.com.cams7.groovy.sample
 
 class QuotedIdentifiers {
 
-	static void printMap1(){
+	//Quoted identifiers
+	static void quotedIdentifiers(){
 		def map = [:]
 
 		map."an identifier with a space and double quotes" = "ALLOWED"
@@ -11,15 +12,6 @@ class QuotedIdentifiers {
 		assert map."an identifier with a space and double quotes" == "ALLOWED"
 		assert map.'with-dash-signs-and-single-quotes' == "ALLOWED"
 
-		println "map1 values:"
-		map.each {k,v ->
-			println "$k = $v"
-		}
-	}
-
-	static void printMap2(){
-		def map = [:]
-
 		map.'single quote' = "abc"
 		map."double quote" = 1
 		map.'''triple single quote''' = 2.5
@@ -27,29 +19,19 @@ class QuotedIdentifiers {
 		map./slashy string/ = [1, 2, 3]
 		map.$/dollar slashy string/$ = null
 
-		println "map2 values:"
-		map.each {k,v ->
-			println "$k = $v"
-		}
-	}
-	
-	static void printMap3(){
-		def map = [:]
-		
 		def firstname = "Homer"
 		map."Simson-${firstname}" = "Homer Simson"
-		
+
 		assert map.'Simson-Homer' == "Homer Simson"
-		
-		println "map3 values:"
+
+		println "map values:"
 		map.each {k,v ->
 			println "$k = $v"
 		}
 	}
 
+
 	static void main(String[] args) {
-		printMap1()
-		printMap2()
-		printMap3()
+		quotedIdentifiers()
 	}
 }
