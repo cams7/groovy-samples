6b515a5e0819641c2191076826c3f31b06048e04
César Magalhães <ceanma@gmail.com>
2015-08-05 13:47:34 -0300

    Foi executado os exemplos '1.1.6. Booleans', '1.1.7. Lists', '1.1.8. Arrays' e '1.1.9. Maps'

---
+++ arrays/src/main/groovy/br/com/cams7/groovy/sample/ArrayTest.groovy
@@ -0,0 +1,34 @@
+package br.com.cams7.groovy.sample
+
+class ArrayTest {
+
+	static void main(String[] args) {
+		String[] arrStr = ['Ananas', 'Banana', 'Kiwi']
+
+		assert arrStr instanceof String[]
+		assert !(arrStr instanceof List)
+
+		def numArr = [1, 2, 3] as int[]
+
+		assert numArr instanceof int[]
+		assert numArr.size() == 3
+
+		println "array values:"
+		arrStr.each {print "$it " }
+		println "\narray values:"
+		numArr.each {print "$it " }
+
+		def matrix3 = new Integer[3][3]
+		assert matrix3.size() == 3
+
+		Integer[][] matrix2
+		matrix2 = [[1, 2], [3, 4]]
+		assert matrix2 instanceof Integer[][]
+
+		String[] names = ['Cédric', 'Guillaume', 'Jochen', 'Paul']
+		assert names[0] == 'Cédric'
+
+		names[2] = 'Blackdrag'
+		assert names[2] == 'Blackdrag'
+	}
+}
---
+++ booleans/src/main/groovy/br/com/cams7/groovy/sample/BooleanTest.groovy
@@ -0,0 +1,11 @@
+package br.com.cams7.groovy.sample
+
+class BooleanTest {
+
+	static void main(String[] args) {
+		def myBooleanVariable = true
+		boolean untypedBooleanVar = false
+		println "$myBooleanVariable, $untypedBooleanVar"
+	}
+
+}
